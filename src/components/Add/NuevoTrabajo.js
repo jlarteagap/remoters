70dbf65a6c6cb7575bcebbafcbf789a7c843ed3b
Jorge <jorgelartearteagap@gmail.com>
2021-03-20 17:49:45 -0400

    validaciones en el formulario de agregar trabajo

--- src/components/Add/NuevoTrabajo.js
+++ src/components/Add/NuevoTrabajo.js
@@ -5,10 +5,21 @@ import { withRouter } from 'react-router-dom'
 import Sidebar from '../Sidebar/Sidebar'
 import './Forms.css'
 
+import Error from '../General/Error'
+
 const NuevoTrabajo = (props) => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState(
+        {company: '',
+        position: '',
+        category: '', 
+        city: '', 
+        link: '', 
+        remote: '', 
+        email: ''}
+    )
     const [isRemote, setRemote] = useState(false)
+    const [error, setError] = useState({error: false})
 
     const changeHandler = () => {
         setRemote(!isRemote);
@@ -28,10 +39,24 @@ const NuevoTrabajo = (props) => {
         >
             {newJob => (
                 <div className="content">
+                    
                     <form className="form"
                         onSubmit={e => {
                             e.preventDefault();
                             const { company, position, category, city, link, remote, email } = data
+                            
+                            // Validations
+                            if(company === ''|| position === '' || category === '' || city === '' || link === ''|| remote === '' || email === ''){
+                                setError({
+                                    error: true
+                                })
+                                return
+                            }
+                            setError({
+                                error: false
+                            })
+                            
+                            // SEND DATA TO MONGO 
                             const input = {
                                 company,
                                 position,
@@ -55,7 +80,7 @@ const NuevoTrabajo = (props) => {
                                 placeholder="Ej: Acme Corp"
                                 name="company"
                                 onChange={inputData}
-                                required
+                                
                             />
                         </div>
                         <div className="form__group">
@@ -64,12 +89,12 @@ const NuevoTrabajo = (props) => {
                             type="text" 
                             placeholder="Ej: Product Manager" 
                             name="position" onChange={inputData}
-                            required
+                            
                             />
                         </div>
                         <div className="form__group">
                             <label>Categoria</label>
-                            <select name="category" onChange={inputData} required>
+                            <select name="category" onChange={inputData} >
                                 <option value="">Elegir...</option>
                                 <option value="SOFTWARE_DEVELOP">Software Develop</option>
                                 <option value="SOCIAL_MEDIA">Social Media</option>
@@ -79,7 +104,7 @@ const NuevoTrabajo = (props) => {
                         </div>
                         <div className="form__group">
                             <label>Ciudad</label>
-                            <select name="city" onChange={inputData} required>
+                            <select name="city" onChange={inputData} >
                                 <option value="">Elegir...</option>
                                 <option value="SANTA_CRUZ">Santa Cruz</option>
                                 <option value="LA_PAZ">La Paz</option>
@@ -94,7 +119,7 @@ const NuevoTrabajo = (props) => {
                         </div>
                         <div className="form__group">
                             <label>Enlace para postular</label>
-                            <input type="text" placeholder="http://" name="link" onChange={inputData} required/>
+                            <input type="text" placeholder="http://" name="link" onChange={inputData} />
                         </div>
                         <div className="form__group">
                             <label className="remote">
@@ -115,8 +140,9 @@ const NuevoTrabajo = (props) => {
                             <label>Email de contacto</label>
                             <input type="text" placeholder="" name="email" onChange={inputData} />
                         </div>
-
-                        <button type="submit" className="btn">Enviar publicación</button>
+                        { error.error ? <Error message="Todos los campos deben ser llenados correctamente" /> : ''}
+                        
+                        <button type="submit" className="btn"> Enviar publicación</button>
 
                     </form>
                 </div>
