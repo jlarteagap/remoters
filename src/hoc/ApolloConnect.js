363d9dd22ba5e00b856bae7e5998af2b3a1f95e2
Jorge <jorgelartearteagap@gmail.com>
2021-04-25 16:42:23 -0400

    Default image on jobs and Grid-Loyout format

--- src/hoc/ApolloConnect.js
+++ src/hoc/ApolloConnect.js
@@ -1,7 +1,7 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 export const client = new ApolloClient({
-  uri: "http://localhost/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache({
     addTypename: false
   }),
