9808dfd2ec2fe9f52007f39e2530337c85ad1581
Jorge <jorgelartearteagap@gmail.com>
2021-05-18 17:56:54 -0400

    Change name

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import Hero from '../Hero/Hero'
 
 const Layout = (props) => (
         <Fragment>
-            <Header reset = {props.reset} title="CRUITS" />
+            <Header reset = {props.reset} title="nEmpleos" />
             <Hero />
                 {props.children}
             <Footer />
