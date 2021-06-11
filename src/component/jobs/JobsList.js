ae752f5e6cf3520977a66b775d7a3df966a8db78
Jorge <jorgelartearteagap@gmail.com>
2021-06-10 21:25:47 -0400

    firts commit and firts connect whit server

---
+++ src/component/jobs/JobsList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Jobs.css'
+import Job from './Job'
+import { useQuery } from "@apollo/client"
+import { GET_JOBS } from '../../queries'
+
+const JobsList = () => {
+    const { loading, error, data } = useQuery(GET_JOBS)
+    if(loading) return "Loading"
+    if(error) return "Error: "
+
+    // return data.getJobs.map(({ company, id}) => (
+    //     <p key={id}>{company}</p>
+    // )
+    // )
+    return(
+        <div>
+            {data.getJobs.map(job => {
+                return(
+                    <Job
+                        key={job.id}
+                        job={job}
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export default JobsList
---
+++ src/queries/index.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const GET_JOBS = gql`
+    query getJobs($limit:Int, $offset:Int){
+        getJobs(limit: $limit, offset:$offset){
+          id
+          company
+          position
+          city
+          link
+          category
+          remote
+        }
+      }
+`;
