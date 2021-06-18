adc637ccc290b7f6341f9e1d4c66a0b01e01d901
Jorge <jorgelartearteagap@gmail.com>
2021-06-18 18:15:41 -0400

    add set context on ApolloClient to connect with the server

--- src/hoc/ApolloConnect.js
+++ src/hoc/ApolloConnect.js
@@ -1,5 +1,7 @@
-import { ApolloClient, InMemoryCache, from, HttpLink} from "@apollo/client";
+import { ApolloClient, InMemoryCache, from, createHttpLink, HttpLink} from "@apollo/client";
+// import { setContext } from "apollo-link-context";
 import { onError} from '@apollo/client/link/error'
+import { setContext } from '@apollo/client/link/context';
 
 const errorLink = onError(({graphqlErrors, nerworkError}) => {
     if(graphqlErrors){
@@ -9,12 +11,22 @@ const errorLink = onError(({graphqlErrors, nerworkError}) => {
     }
 })
 
-const link = from([
-    errorLink,
-    new HttpLink({uri: "http://localhost:4000/graphql"})
-])
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+  });
+  
+const authLink = setContext((_, { headers })=> {
+    const token = localStorage.getItem('token')
+
+    return {
+        headers: {
+            ...headers, 
+            authorization: token ? token : ''
+        }
+    }
+})
 
 export const client = new ApolloClient({
-    link,
-    cache: new InMemoryCache()
-});
+    link: authLink.concat(httpLink, errorLink),
+    cache: new InMemoryCache(),
+  });
