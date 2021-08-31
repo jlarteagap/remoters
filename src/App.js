5d12b8c4d9eb90195a68333eaaeb7ed7fccd4180
Jorge <jorgelartearteagap@gmail.com>
2021-08-31 13:12:34 -0400

    Login & Register authenticated redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import React, { useContext }from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Layout from './component/layout/Layout'
 import AppContext from './context/AppContext'
+import AuthRoute from './utils/AuthRoute'
 import { usePagination } from './hooks/usePagination' 
-import { AuthProvider } from './context/auth'
+import { AuthProvider, AuthContext } from './context/auth'
 import Home from './containers/Home'
 import RegisterView from './containers/RegisterView';
 import LoginView from './containers/LoginView';
 
-// import NewJob from './container/NewJob';
+import NewJob from './containers/NewJob';
 // import CategoriesList from "./container/CategoriesList"
 
 import './assets/css/stl.css'
 
 const App = () => {
   const pagination = usePagination()
+  const { user } = useContext(AuthContext)
 
   return (
     <AuthProvider>
@@ -28,13 +29,9 @@ const App = () => {
                 <Route exact path="/">
                   <Home />
                 </Route>
-                <Route exact path="/registro">
-                  <RegisterView />
-                </Route>
-                <Route exact path="/login" render={() => <LoginView /> } />
-                {/* <Route exact path="/agregar">
-                  <NewJob />
-                </Route> */}
+                <AuthRoute exact path="/registro" component={RegisterView} />
+                <AuthRoute exact path="/login" component={LoginView} />
+                {/* {user ? <Route path="/agregar" component={NewJob} /> : <Redirect to="/login" /> } */}
                 {/* <Route exact path="/:category">
                   <CategoriesList
                     nextPage={nextPage}
