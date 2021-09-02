087276a66d8200363f6beec7ba3837fd7214f384
Jorge <jorgelartearteagap@gmail.com>
2021-09-01 22:47:37 -0400

    Restricted and private urls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useContext }from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from './component/layout/Layout'
 import AppContext from './context/AppContext'
-import AuthRoute from './utils/AuthRoute'
+import {AuthRoute, PrivateRoute} from './utils/AuthRoute'
 import { usePagination } from './hooks/usePagination' 
-import { AuthProvider, AuthContext } from './context/auth'
+import { AuthProvider } from './context/auth'
 import Home from './containers/Home'
 import RegisterView from './containers/RegisterView';
 import LoginView from './containers/LoginView';
@@ -16,7 +16,6 @@ import './assets/css/stl.css'
 
 const App = () => {
   const pagination = usePagination()
-  const { user } = useContext(AuthContext)
 
   return (
     <AuthProvider>
@@ -26,20 +25,10 @@ const App = () => {
           <main className="main">
             <div className="container add">
               <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <AuthRoute exact path="/registro" component={RegisterView} />
-                <AuthRoute exact path="/login" component={LoginView} />
-                {/* {user ? <Route path="/agregar" component={NewJob} /> : <Redirect to="/login" /> } */}
-                {/* <Route exact path="/:category">
-                  <CategoriesList
-                    nextPage={nextPage}
-                    prevPage={prevPage}
-                    page={page}
-                    reset={resetState}
-                  />
-                </Route> */}
+                <AuthRoute exact path="/" component={Home} />
+                <AuthRoute exact path="/registro" restricted={true} component={RegisterView} />
+                <AuthRoute exact path="/login" restricted={true} component={LoginView} />
+                <PrivateRoute exact path="/agregar" component={NewJob} />
               </Switch>
             </div>
           </main>
