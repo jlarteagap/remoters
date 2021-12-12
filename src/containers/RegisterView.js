52c650af8baf6bc900f78bdf001bb0cd6c1fc3b7
Jorge <jorgelartearteagap@gmail.com>
2021-12-12 19:15:32 -0400

    [STYLE] Form login / register

--- src/containers/RegisterView.js
+++ src/containers/RegisterView.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Register from '../component/login/Register'
 
-const RegisterView = () => <Register />
+const RegisterView = () => (
+  <div className="login__content">
+    <Register />
+  </div>
+)
 
 export default RegisterView
