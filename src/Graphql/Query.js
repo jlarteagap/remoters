d3ddb184bfdb48501f8dfe88a4694368c4c1f101
Jorge <jorgelartearteagap@gmail.com>
2021-12-13 19:04:56 -0400

    [FIX] Query jobs and categories

--- src/Graphql/Query.js
+++ src/Graphql/Query.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 export const GET_JOBS = gql`
-  query {
-    getJobs {
+  query getJobs($category: String, $limit: Int, $offset: Int) {
+    getJobs(category: $category, limit: $limit, offset: $offset) {
       id
       position
       category
