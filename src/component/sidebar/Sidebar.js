82d46fd3970cafee041ed2c09805a2752e5498ae
Jorge <jorgelartearteagap@gmail.com>
2022-02-04 16:22:25 -0400

    [REFACTOR] Sidebar links

--- src/component/sidebar/Sidebar.js
+++ src/component/sidebar/Sidebar.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import CategoryMenu from './CategoryMenu'
-
+import { GET_JOBS } from '../../Graphql/Query'
 import PropTypes from 'prop-types'
 import './sidebar.css'
-
+import { useQuery } from '@apollo/client'
+import Loading from '../utils/Loading'
 const Sidebar = props => {
+  const { loading, error, data } = useQuery(GET_JOBS)
+
+  if (loading) return <Loading />
+  if (error) return `Error: ${error.message}`
+
   return (
     <div className="sidebar">
-      <CategoryMenu reset={props.reset} />
+      <div className="categories box">
+        {data.getJobs.map(company => {
+          return (
+            <CategoryMenu key={company.id} reset={props.reset} data={company} />
+          )
+        })}
+      </div>
     </div>
   )
 }
