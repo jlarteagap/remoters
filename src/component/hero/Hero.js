4212a9bf49de0efa13e2bfdc2db881c2df672ea6
Jorge <jorgelarteagap@gmail.com>
2022-02-12 22:55:01 -0400

    [FEAT] Hero compponent and animation scroll

---
+++ src/component/hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './hero.css'
+const Hero = () => (
+  <section className="hero is-fullheight is-relative">
+    <div className="hero-body">
+      <div className="has-text-centered">
+        <p className="title">
+          Principal bolsa de trabajo para desarrolladores, creativos y todo
+          profesional en tecnología
+        </p>
+        <p className="subtitle mt-5">
+          Especializada en oportunidades y talento técnico remoto y local
+        </p>
+        <div className="is-flex is-justify-content-center pt-5">
+          <div className="icon-scroll"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Hero
