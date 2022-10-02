65a588b7f2d6ccc581c3e13c29919f72febab198
Jorge <jorgelarteagap@gmail.com>
2022-10-01 23:21:48 -0400

    [REFACTOR] Context
    - Add Apollo Provider
    - Context Auth
    - Pagination Hooks
    - config eslint and prettier
    :

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
 /* eslint-disable react/prop-types */
 // import App from 'next/app'
 import React from 'react'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../src/hoc/ApolloConnect'
+import {usePagination} from '../src/hooks/usePagination'
+import {AuthProvider} from '../src/context/auth'
+
 import 'bulma/css/bulma.min.css'
 import '../public/css/stl.css'
 import '../public/css/fonts.css'
+import AppContext from '../src/context/AppContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const pagination = usePagination()
+
+  return <ApolloProvider client={client}>
+    <AppContext.Provider value={pagination}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  </ApolloProvider>
 }
 
 export default MyApp
