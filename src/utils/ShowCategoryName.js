766b32f312718ed8ffe4eb6c94eea17af36d99e7
Jorge <jorgelarteagap@gmail.com>
2022-10-19 16:10:10 -0400

    [FEAT] get data from querys data

--- src/utils/ShowCategoryName.js
+++ src/utils/ShowCategoryName.js
@@ -1,10 +1,7 @@
-import { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_CATEGORY } from '../Graphql/Query'
 
 const ShowCategoryName = ({ category }) => {
-  const [showCategory, setShowCategory] = useState('')
-
   const { data, loading } = useQuery(GET_CATEGORY, {
     variables: {
       slug: category
@@ -12,13 +9,8 @@ const ShowCategoryName = ({ category }) => {
   })
 
   if (loading) return 'cargando...'
-  useEffect(() => {
-    if (data.getCategory !== undefined || null) {
-      setShowCategory(data.getCategory.name)
-    }
-  }, [category])
 
-  return showCategory
+  return data.getCategory.name
 }
 
 export default ShowCategoryName
