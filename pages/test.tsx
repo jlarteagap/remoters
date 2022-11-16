8295d8a4b6c945fca4df226e3a645c6cca49063c
Jorge <jorgelarteagap@gmail.com>
2022-11-16 17:43:23 -0400

    [TRY] getStaticProps to test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,32 +1,55 @@
 import React from 'react'
 import { gql } from '@apollo/client'
 import client from '../src/hoc/apollo-client'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { GetJobsQuery } from '../service/graphql/graphql'
 
-export const getServerSideProps = async () => {
-  const { data } = await client.query({
-    query: gql`
-      query getJobs {
-        getJobs {
-          slug
+export const getStaticProps: GetStaticProps<{
+  jobs: GetJobsQuery[]
+}> = async () => {
+  try {
+    const res = await client.query({
+      query: gql`
+        query getJobs {
+          getJobs {
+            slug
+          }
         }
-      }
-    `
-  })
+      `
+    })
+    if (res.data == null) {
+      throw new Error('Error Failed to request')
+    }
 
-  return {
-    props: {
-      data
+    const jobs = res.data.getJobs
+    return {
+      props: {
+        jobs
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      props: {
+        jobs: []
+      }
     }
   }
 }
 
-export default async function TestPage({ data }) {
+const TestPage = ({ jobs }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(jobs)
   return (
     <div>
-      {data &&
-        (await data.getJobs.map(item => {
+      {jobs.map(job => {
+        return <li key={job.slug}>{job.slug}</li>
+      })}
+      {/* {data &&
+        data.getJobs.map(item => {
           return <li key={item.slug}>{item.slug}</li>
-        }))}
+        })} */}
     </div>
   )
 }
+
+export default TestPage
