b30d706c9ac87e0e6adfd3bcc18d4f75350fb7f0
Jorge <jorgelarteagap@gmail.com>
2022-12-05 23:53:20 -0400

    SEO fix

--- pages/categoria/[category].tsx
+++ pages/categoria/[category].tsx
@@ -3,20 +3,12 @@ import Sidebar from '@components/sidebar/Sidebar'
 import Categories from '@components/jobs/Categories'
 
 import { useRouter } from 'next/router'
-import Seo from '@components/seo/seo'
 
 const CategoriesList = () => {
   const router = useRouter()
-  const path: any = router.query.category
+
   return (
     <>
-      <Seo
-        title={path.replace('_', ' ')}
-        description={`Encuentra y postula a trabajos como ${path.replace(
-          '_',
-          ' '
-        )}`}
-      />
       <div className="home">
         <div className="home__jobs">
           <Categories category={router.query.category} />
