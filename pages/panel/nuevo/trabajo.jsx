2808afff109b43fecd846f74bc94e7b10f2e05e6
Jorge <jorgelarteagap@gmail.com>
2022-12-05 23:47:18 -0400

    SEO

--- pages/panel/nuevo/trabajo.jsx
+++ pages/panel/nuevo/trabajo.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import AddJobs from '@components/add/AddJobs.js'
-import Seo from '@components/seo/Seo'
+
 import withAuth from '@hoc/withAuth'
 const trabajo = () => {
-  return (
-    <>
-      <Seo title="Agregar nuevo trabajo" />
-      <AddJobs />
-    </>
-  )
+  return <AddJobs />
 }
 
 export default withAuth(trabajo)
