64e8d9548987a8d79e4e700ba91f925316a59aa9
Jorge <jorgelarteagap@gmail.com>
2023-08-29 14:47:36 -0400

    [FIX] Change data for new subdomain project

--- next.cofig.js
+++ next.cofig.js
@@ -2,5 +2,19 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_APP_SERVER: process.env.NEXT_APP_SERVER
+  },
+  rewrites(){
+    return{
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{
+            type: 'host',
+            value: 'jobs.jlarteaga.com'
+          },],
+          destination: '/app/:path',
+        }
+      ]
+    }
   }
 }
